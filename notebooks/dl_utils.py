d86b48a713268e6772b5064815bb8e4d89ff97ab
sevrietv <seppe.vanrietvelde@outlook.com>
2025-03-27 19:52:21 +0100

    Data exploration

--- notebooks/dl_utils.py
+++ notebooks/dl_utils.py
@@ -15,9 +15,9 @@ def extract_data(path):
             for file in os.listdir(folder_path):
                 file_path = os.path.join(folder_path, file)
 
-                # Load image and append to dataset
+                # Load image, normalize (0 to 1 scale), and append to dataset
                 img = Image.open(file_path)
-                X_array.append(np.asarray(img))
+                X_array.append(np.asarray(img) / 255)
                 y_array.append(folder)  # Label is the folder name
 
     return np.asarray(X_array), np.asarray(y_array)
