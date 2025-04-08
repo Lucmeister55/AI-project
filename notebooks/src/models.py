bbe2081f4ee782c2c7457e34531e250a39ddd621
Lucmeister55 <96026889+Lucmeister55@users.noreply.github.com>
2025-04-08 18:36:58 +0200

    Updated env

--- notebooks/src/models.py
+++ notebooks/src/models.py
@@ -1,9 +1,10 @@
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, Input
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import EarlyStopping
 
-def custom_CNN(img_shape, dropout_rate, filters, lr, dense_unit):
+def custom_CNN(img_shape, dropout_rate, filters, dense_unit, lr, metrics):
     model = Sequential([
         Input(shape=img_shape),
         Conv2D(filters, 3, padding='same', activation='relu'),
@@ -18,5 +19,11 @@ def custom_CNN(img_shape, dropout_rate, filters, lr, dense_unit):
         Dense(dense_unit, activation='relu'),
         Dense(1, activation='sigmoid')
     ])
+
+    # Create optimizer with the given learning rate.
+    optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
+    model.compile(optimizer=optimizer,
+                    loss=tf.keras.losses.BinaryCrossentropy(),
+                    metrics=metrics)
 
     return model
